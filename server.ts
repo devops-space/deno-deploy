b67d2004885ed639615f2baf66185967034f715d
tym <1772314831@qq.com>
2022-02-21 10:21:36 +0800

    tip

--- server.ts
+++ server.ts
@@ -1,10 +1,11 @@
 import { serve } from 'https://deno.land/std@0.120.0/http/server.ts'
-import { readText } from './util.js'
+import { readText } from './util.ts'
 
-async function handler(req) {
+async function handler(req: Request) {
+  console.log(req.url)
   const text = await readText('./README.md')
   return new Response(text)
 }
 
 console.log('Listening on http://localhost:8000')
-await serve(handler)
+await serve(handler, { hostname: '0.0.0.0', port: 8000 })
--- util.ts
+++ util.ts
@@ -1,4 +1,4 @@
-export async function readText(path) {
+export async function readText(path: string): Promise<string> {
   const data = await Deno.readFile(path)
   const decoder = new TextDecoder('utf-8')
   return decoder.decode(data)
