3907fd26a2102c4637797e18c0415c4c94c8dd7d
tym <1772314831@qq.com>
2023-12-09 10:58:35 +0800

    sync

--- address/main.ts
+++ address/main.ts
@@ -2,9 +2,9 @@ import { serve } from 'https://deno.land/std@0.120.0/http/server.ts'
 
 
 async function handler(req: Request) {
-  const resp = await fetch('https://www.searchpeoplefree.com/phone-lookup/706-990-8659', {
+  const resp = await fetch('https://page404.cn', {
     headers: {
-      userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
+      'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
       referer: 'https://www.searchpeoplefree.com/phone-lookup',
       cookie: '',
       accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
@@ -16,7 +16,7 @@ async function handler(req: Request) {
   return new Response(resp.body, {
     status: resp.status,
     headers: {
-      contentType: 'text/plain',
+      'content-type': 'text/plain',
     }
   })
 }
