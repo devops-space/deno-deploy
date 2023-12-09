a81f0731466c7964a43448747db177e608cfc555
tym <1772314831@qq.com>
2023-12-09 10:52:45 +0800

    sync

--- address/main.ts
+++ address/main.ts
@@ -6,6 +6,8 @@ async function handler(req: Request) {
     headers: {
       userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
       referer: 'https://www.searchpeoplefree.com/phone-lookup',
+      cookie: '',
+      accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
     }
   })
 
