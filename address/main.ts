e47d558886c8da6a3ec6f847b87c2dda3be634d8
tym <1772314831@qq.com>
2023-12-09 11:18:42 +0800

    sync

--- address/main.ts
+++ address/main.ts
@@ -2,13 +2,15 @@ import { serve } from 'https://deno.land/std@0.120.0/http/server.ts'
 
 
 async function handler(req: Request) {
-  const resp = await fetch('https://www.searchpeoplefree.com/phone-lookup/706-990-8659', {
+  const resp = await fetch('https://www.searchpeoplefree.com/api/v3/getslots', {
+    method: 'POST',
     headers: {
       'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
-      referer: 'https://www.searchpeoplefree.com/phone-lookup',
-      cookie: '',
+      referer: 'https://www.searchpeoplefree.com/phone-lookup/706-990-8659',
       accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-    } 
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ "page": "Phone_Results", "last": "", "first": "", "phone": "7069908659", "email": "", "unit": "", "house": "", "street": "", "line1": "", "city": "", "state": "" })
   })
 
 
