de006a3574ccc38aeebcac5fc9092a582e002695
tym <1772314831@qq.com>
2023-12-09 10:47:14 +0800

    sync

--- address/main.ts
+++ address/main.ts
@@ -2,7 +2,12 @@ import { serve } from 'https://deno.land/std@0.120.0/http/server.ts'
 
 
 async function handler(req: Request) {
-  const resp = await fetch('https://www.searchpeoplefree.com/phone-lookup/706-990-8659')
+  const resp = await fetch('https://www.searchpeoplefree.com/phone-lookup/706-990-8659', {
+    headers: {
+      userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
+      referer: 'https://www.searchpeoplefree.com/phone-lookup',
+    }
+  })
   const headers = new Headers(resp.headers)
   return new Response(resp.body, {
     status: resp.status,
